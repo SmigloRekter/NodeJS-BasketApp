b4eca29c7beee98bbdaa75e8d277b6aabdaf0189
Adam <smiglo196@gmail.com>
2019-02-21 14:20:35 +0100

    Added checking if user participates in game

--- index.js
+++ index.js
@@ -39,34 +39,6 @@ app.use(function (req, res, next) {
    next();
 });
 
-let options = {
-    swaggerDefinition: {
-        info: {
-            description: 'This is a sample server',
-            title: 'Swagger',
-            version: '1.0.0',
-        },
-        host: 'localhost:5000',
-        basePath: '/',
-        produces: [
-            "application/json",
-            "application/xml"
-        ],
-        schemes: ['http', 'https'],
-        securityDefinitions: {
-            JWT: {
-                type: 'apiKey',
-                in: 'header',
-                name: 'Authorization',
-                description: "",
-            }
-        }
-    },
-    basedir: __dirname, //app absolute path
-    files: ['./GameController.js'] //Path to the API handle folder
-};
-expressSwagger(options);
-
 app.use(require('./routes'));
 app.use(LoggerMiddleware);
 
